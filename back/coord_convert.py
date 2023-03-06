602d85275b87020bf1a371a575839868995baf99
h0800 <jh0893@tamu.edu>
2023-03-06 10:07:23 -0600

    parsing data

--- back/coord_convert.py
+++ back/coord_convert.py
@@ -3,13 +3,24 @@ import requests
 import json
 r = requests.get('https://transport.tamu.edu/BusRoutesFeed/api/route/12/pattern', auth=('user', 'pass'))
 print(r.status_code)
-longitude = []
-latitude = []
+#longitude = []
+#latitude = []
+i = 0
 for j in r.json():
-    longitude.append(j.get("Latitude"))
-    latitude.append(j.get("Longtitude"))
-
-for i in range(len(latitude)):
+    input = ""
+    input += j.get("Name") + ", "
+    input += str(j.get("Rank")) + ", "
     transformer = Transformer.from_crs(102100, 4326)
-    coords = transformer.transform(latitude[i],longitude[i])
-    print(coords)
+    (lat, long) = transformer.transform(j.get("Longtitude"), j.get("Latitude"))
+    input += str(lat) + ", "
+    input += str(long)
+    if (i < 100):
+        print(input)
+        i += 1
+    #longitude.append(j.get("Latitude"))
+    #latitude.append(j.get("Longtitude"))
+
+#for i in range(len(latitude)):
+#    transformer = Transformer.from_crs(102100, 4326)
+ #   coords = transformer.transform(latitude[i],longitude[i])
+    #print(coords)
