33818bef848167150b751ec39326b7eb25275479
yocuml19 <yocuml19@tamu.edu>
2023-03-08 09:47:01 -0600

    added route selection to MapClass file

--- front/MapClass.js
+++ front/MapClass.js
@@ -15,27 +15,6 @@ const MSC = {
     longitudeDelta: 0.02,
 }
 
-const MSC2 = {
-    latitude: 30.6125,
-    longitude: -96.3420,
-    latitudeDelta: 0.02,
-    longitudeDelta: 0.02,
-}
-const waypoints = [
-    {latitude: 30.6152035662653, longitude: -96.3374031387212, },
-
-    {latitude: 30.6167807052601, longitude: -96.3350937566217, },
-
-    {latitude: 30.6169081101959, longitude: -96.3351178965043, },
-
-    {latitude: 30.617004669726196, longitude: -96.335121919818, },
-
-    {latitude: 30.617118663616107, longitude: -96.33509643883089, },
-
-    {latitude: 30.61722595198309, longitude: -96.335060229007, },
-];
-
-
 export function Map({ navigation }) {
     return (
         <View style={styles.container}>
@@ -116,3 +95,11 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+
+export function RouteSelection() {
+    return (
+      <View style={styles.link}>
+        <Text>RouteSelection Screen</Text>
+      </View>
+    );
+  }
