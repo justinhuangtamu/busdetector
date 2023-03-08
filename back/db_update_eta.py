42a6dcc10305170787ab1aa0f1475b2a6e23396d
h0800 <jh0893@tamu.edu>
2023-03-08 10:59:49 -0600

    first google api test

---
+++ back/db_update_eta.py
@@ -0,0 +1,16 @@
+from pyproj import Transformer  # coordinate transformations
+import requests  # HTTP requests library
+
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+key = "AIzaSyAu6nlbhY5f261wk7EGJRDpQosx844VG5Q"
+def get_eta(start_point, end_point):
+
+    a = url + "origins="+start_point+"&destinations="+end_point+"&key="+key
+    print(a)
+    r = requests.get(url)
+    print(r.text)
+
+
+s = "30.6152035662653%2C-96.3374031387212"
+d = "30.613800770866902%2C-96.350859782151"
+get_eta(s,d)
