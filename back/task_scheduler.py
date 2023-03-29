f486800a564cf364b16be9dd544d0f62ffca3a1d
Carlos Barker <carlosbarker@outlook.com>
2023-03-28 19:23:38 -0500

    task scheduler v1 (draft), need testing

--- back/task_scheduler.py
+++ back/task_scheduler.py
@@ -13,16 +13,46 @@ import db_update_eta
 import db_update_routes
 
 # import libraries
-from datetime import datetime
 import time
-import schedule
+import signal
+
+# global vars
+exit = False
+
+
+# ctrl-c signal handler
+def handler(signum, frame):
+    global exit
+    exit = True
+    print("[HANDLER] CTRL-C PRESSED. SAFELY QUITTING!")
 
 
 # run at startup
+signal.signal(signal.SIGINT, handler)
+print("[" + time.localtime() + "] Updating routes... ", end="")
 db_update_routes.update_routes()
+print("DONE\n")
 
-schedule.every(15).seconds.do(db_update_buses.update_buses())
-schedule.every(15).seconds.do(db_update_eta.update_etas())
+# periodic updates
+routeupdate_flag = False  # only update routes once a day
+while not exit:
+    if (time.localtime().tm_hour > 6 or time.localtime().tm_hour < 2):
+        routeupdate_flag = False
 
-while True:
-    print("Hello")
+        print("[" + time.localtime() + "] Updating bus locations... ", end="")
+        db_update_buses.update_buses()
+        print("DONE")
+
+        print("[" + time.localtime() + "] Updating bus ETAs... ", end="")
+        db_update_eta.update_etas()
+        print("DONE")
+
+        time.sleep(15)
+    elif (time.localtime().tm_hour < 6 and time.localtime().tm_hour > 2 and routeupdate_flag is not True):
+        routeupdate_flag = True
+
+        print("[" + time.localtime() + "] Updating routes... ", end="")
+        db_update_routes.update_routes()
+        print("DONE\n")
+    else:
+        time.sleep(1)
